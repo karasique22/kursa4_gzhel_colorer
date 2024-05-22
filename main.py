1cc677abc8a8574e3c2f421704f0a57fc23bfa53
karasique22 <timofeidudin@mail.ru>
2024-05-23 00:14:59 +0300

    add some ui

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import ttk
 from sklearn.cluster import KMeans
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageTk
 import itertools
 
 
@@ -83,35 +84,87 @@ def index_and_recolor_images():
         messagebox.showerror("Ошибка", str(e))
 
 
-# Создание интерфейса
+def open_explanatory_popup():
+    messagebox.showinfo("Для чего нужна эта программа?",
+                        "Программа предназначена для работы с изображениями, преобразуя их цветовую палитру "
+                        "в соответствии с заданной схемой, и в данном случае это палитра Гжель – традиционный "
+                        "русский стиль росписи, использующий оттенки синего на белом фоне.\n\n"
+                        "Использование программы:\n"
+                        "• Запустите программу. В главном окне вы увидите интерфейс с кнопками и полями "
+                        "для выбора изображения и папки назначения.\n"
+                        "• С помощью кнопок \"Выберите исходное изображение\" и \"Выберите папку назначения\" "
+                        "укажите необходимые пути.\n"
+                        "• После выбора нажмите кнопку \"Перекрасить!\", чтобы начать процесс индексации и перекраски.\n"
+                        "• Программа отобразит сообщение об успешном завершении процесса или сообщение об ошибке, "
+                        "если что-то пойдет не так.")
+
+
 root = tk.Tk()
+# Используйте тему оформления 'clam'
+style = ttk.Style(root)
+style.theme_use('clam')
+
+# Определите кастомные шрифты и цвета
+customFont = ('Roboto', 16)
+buttonFont = ('Roboto', 16, 'bold')
+backgroundColor = '#CFDEFF'
+buttonColor = '#d0d0f0'
+textColor = '#333'
+
+# Настройте корневое окно
+root.configure(bg=backgroundColor)
 root.title("Колорирование в палитру Гжели")
 
-frame = tk.Frame(root)
-frame.pack(padx=10, pady=10)
+pic = Image.open('pic.png')
+pic = pic.resize((300, 300))
 
-label_source_path = tk.Label(frame, text="Путь к исходному изображению:")
-label_source_path.pack(fill='x', expand=True)
+question = Image.open('question.png')
+question = question.resize((50, 50))
 
-entry_source_path = tk.Entry(frame, width=50)
+
+pic_image = ImageTk.PhotoImage(pic)
+question_image = ImageTk.PhotoImage(question)
+palette_image = tk.PhotoImage(file='palette.png')
+
+pic_label = tk.Label(root, image=pic_image, bg=backgroundColor)
+palette_label = tk.Label(root, image=palette_image)
+
+pic_label.pack(padx=20, pady=10)
+
+frame = tk.Frame(root, bg=backgroundColor)
+frame.pack(padx=20, pady=20)
+
+label_source_path = tk.Label(frame, text="Путь к исходному изображению:",
+                             bg=backgroundColor, fg=textColor, font=customFont)
+label_source_path.pack(fill='x', expand=True, pady=(0, 10))
+
+entry_source_path = tk.Entry(frame, font=customFont)
 entry_source_path.pack(fill='x', expand=True)
 
 button_browse_source = tk.Button(
-    frame, text="Выбрать", command=select_source_image)
-button_browse_source.pack(fill='x', expand=True)
+    frame, text="Выбрать", command=select_source_image, bg=buttonColor, font=buttonFont)
+button_browse_source.pack(fill='x', expand=True, pady=5)
 
-label_destination_path = tk.Label(frame, text="Путь к папке назначения:")
-label_destination_path.pack(fill='x', expand=True)
+label_destination_path = tk.Label(
+    frame, text="Путь к папке назначения:", bg=backgroundColor, fg=textColor, font=customFont)
+label_destination_path.pack(fill='x', expand=True, pady=(10, 10))
 
-entry_destination_path = tk.Entry(frame, width=50)
+entry_destination_path = tk.Entry(frame, font=customFont)
 entry_destination_path.pack(fill='x', expand=True)
 
 button_browse_destination = tk.Button(
-    frame, text="Выбрать", command=select_destination_folder)
-button_browse_destination.pack(fill='x', expand=True)
+    frame, text="Выбрать", command=select_destination_folder, bg=buttonColor, font=buttonFont)
+button_browse_destination.pack(fill='x', expand=True, pady=5)
 
 button_recolor = tk.Button(
-    frame, text="Перекрасить!", command=index_and_recolor_images)
-button_recolor.pack(fill='x', expand=True)
+    frame, text="Перекрасить!", command=index_and_recolor_images, bg=buttonColor, font=buttonFont)
+button_recolor.pack(fill='x', expand=True, pady=(30, 15))
+
+palette_label.pack(padx=20, pady=10)
+
+question_button = tk.Button(
+    root, image=question_image, command=open_explanatory_popup, background=backgroundColor, border=0)
+question_button.pack(padx=20, pady=20)
+
 
 root.mainloop()
